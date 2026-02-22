7bf985a4d9589a74fbfe7713a1db5ab793ff1c46
Om Gharge <om.gharge@techverito.com>
2026-02-23 00:58:32 +0530

    ADD: dressing and lualine

--- lua/custom/core/lazy.lua
+++ lua/custom/core/lazy.lua
@@ -1,1 +1,3 @@
 vim.schedule(function() require('lazy.core.config').options.change_detection.notify = false end)
+vim.schedule(function() require('lazy.core.config').options.checker.notify = false end)
+vim.schedule(function() require('lazy.core.config').options.checker.enabled = true end)
