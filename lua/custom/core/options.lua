5674b89ad895e2566ac9f19419cbbb47f1d0c760
Om Gharge <om.gharge@techverito.com>
2026-02-22 20:33:16 +0530

    ADD: Custom Key mappings for file explorer and insert mode

--- lua/custom/core/options.lua
+++ lua/custom/core/options.lua
@@ -1,15 +1,16 @@
 vim.g.netrw_liststyle = 3
 
+-- [[CUSTOM KEY MAPS]]
+
 local keymap = vim.keymap.set
 
--- Toggle file explorer
+-- [INPUT]
+-- 1. Toggle insert mode
+keymap('i', 'jk', '<ESC>', { desc = 'Exit insert mode with jk' })
+
+-- [FILE EXPLORER - Neo-tree]
+-- 1. Toggle file explorer
 keymap('n', '<leader>ee', '<cmd>Neotree toggle filesystem reveal_force_cwd<CR>', { desc = 'Toggle file explorer' })
 
--- Reveal current file
+-- 2. Reveal current file
 keymap('n', '<leader>ef', '<cmd>Neotree reveal<CR>', { desc = 'Reveal current file in explorer' })
-
--- Collapse explorer
-keymap('n', '<leader>ec', '<cmd>Neotree action=close_all_nodes<CR>', { desc = 'Collapse file explorer' })
-
--- Refresh explorer
-keymap('n', '<leader>er', '<cmd>Neotree refresh<CR>', { desc = 'Refresh file explorer' })
---
+++ lua/custom/plugins/neo-tree.lua
@@ -0,0 +1,10 @@
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+
+  opts = function(_, opts)
+    opts.window = opts.window or {}
+    opts.window.position = 'right'
+
+    return opts
+  end,
+}
